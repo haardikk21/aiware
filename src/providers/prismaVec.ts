6099760e95b7697cc32dd76abaff16139c2ebfc7
mhykes <mhykes@users.noreply.github.com>
2023-08-18 18:07:08 +0000

    Better error detection and handling.

--- src/providers/prismaVec.ts
+++ src/providers/prismaVec.ts
@@ -42,24 +42,76 @@ export async function addDocsToVectorstore(splitDocs: Document[]) {
   
     console.time(`Creating embeddings`);
   
-    await vectorStore.addModels(
-      await db.$transaction(
-        splitDocs.map((doc) =>
-          db.document.create({
-            data: {
-              content: doc.pageContent,
-              repoPath: doc.metadata.repoPath,
-              filePath: doc.metadata.filePath,
-              commitHash: doc.metadata.commitHash,
-            },
-          })
+    const chunks = split(splitDocs);
+    for(let chunk of chunks) {
+      await vectorStore.addModels(
+        await db.$transaction(
+          prepareDocs(chunk, db)
         )
-      )
-    );
+      );  
+    }
   
     console.timeEnd(`Creating embeddings`);
   }
+
+function split( stuff: Document<Record<string, any>>[] ): Document<Record<string, any>>[][]{
+  const size = 1000;
+  let chunks = [];
+  for(let i=0; i<stuff.length; i+=size) {
+    chunks.push(stuff.slice(i,i+size));
+  }
+  return chunks;
+}
+
+function prepareDocs(splitDocs: Document<Record<string, any>>[], db: PrismaClient): Prisma.PrismaPromise<any>[] {
+  return splitDocs.map((doc)=>prepareDoc(doc,db));
+}
+
+function prepareDoc(doc: Document<Record<string, any>>, db: PrismaClient) {
+  const {pageContent} = doc;
+  const {repoPath, filePath, commitHash} = doc.metadata;
+  const allValidUtf8 = [pageContent, repoPath, filePath, commitHash].reduce((result,value)=>{return result && isValidUtf8(value);},true);
+  const hasNullBytes = [pageContent, repoPath, filePath, commitHash].reduce((result,value)=>{return result && hasNullByte(value);},true);
   
+  if ( !allValidUtf8 ) {
+    console.log(`INVALID UTF8: skipping ${doc.metadata.filePath}`);
+  } else if ( hasNullBytes ) {
+    console.log(`NULL BYTES: skipping ${doc.metadata.filePath}`);
+  } else {
+    console.log(`PROCESSING: ${doc.metadata.filePath}`);
+    return db.document.create({
+      data: {
+        content: doc.pageContent,
+        repoPath: doc.metadata.repoPath,
+        filePath: doc.metadata.filePath,
+        commitHash: doc.metadata.commitHash,
+      },
+    });
+  }
+}
+
+function hasNullByte(testString: string): boolean {
+  const buffer = Buffer.from(testString);
+  for (let i = 0; i < buffer.length; i++) {
+    if (buffer[i] === 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isValidUtf8(testString: string): boolean {
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+  try {
+    const encodedData = encoder.encode(testString);
+    const decodedData = decoder.decode(encodedData);
+    return testString == decodedData;
+  } catch (error) {
+    return false;
+  }
+
+}
 
 export async function deleteOldEmbeddingsForDirtyFiles(
     filePaths: string[],
