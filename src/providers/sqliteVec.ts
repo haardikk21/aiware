0f902e98a1a0d18b505ed26cc9b527d68c70f83b
mhykes <mhykes@users.noreply.github.com>
2023-09-11 05:11:24 +0000

    sqliteVec provider works now.

--- src/providers/sqliteVec.ts
+++ src/providers/sqliteVec.ts
@@ -1,41 +1,38 @@
 import { Metadata } from "../types";
 import { Document, DocumentInput } from "langchain/document";
 import { VectorStore } from "langchain/vectorstores/base";
-import SqliteDb,{ Database } from "better-sqlite3";
-import * as sqlite3 from "sqlite3";
-import * as sqlite_vss from "sqlite-vss";
+import SqliteDb, { Database, Statement } from "better-sqlite3";
+import * as sqlite_vss from "sqlite-vss"
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { existsSync, writeFileSync } from "fs";
+import { existsSync, writeFileSync, readFileSync, fstat } from "fs";
 import { Embeddings } from "langchain/embeddings/base";
 
 
-const db = initDb();
-const sqliteVecData = "sqliteVecData.json";
+const { SQLITEVEC_LOGGING } = process.env;
+const sqliteVecData = "sqliteVecData.db";
+
+let db = initDb();
 let vectorStore: SqliteVectorStore;
 
-function initDb() : Database {
-  const db = new SqliteDb(":memory:");
-  sqlite_vss.loadVss(db);
-  sqlite_vss.loadVector(db);
-  const stmt: sqlite3.Statement = db.prepare("select vss_version()");
-  const version = stmt.run();
-  console.log("intialized sqlite db with vss version",version);
-  return db;
+function log(...args) {
+  if (!!SQLITEVEC_LOGGING) console.log(`${SQLITEVEC_LOGGING}`,...args);
 }
 
-function _initDb() : Database {
-  const db: Database = new sqlite3.Database(":memory:");
-  sqlite_vss.loadVss(db);
+function initDb(data? :Buffer ) : Database {
+  const infoQuery = !data?"select vss_version() as vss_version, sqlite_version() as sqlite_version, 'N/A' as vectors;":"select vss_version() as vss_version, sqlite_version() as sqlite_version, COUNT(*) as vectors FROM vss_docs;";
+  const db = new SqliteDb(!data?":memory:":data);
   sqlite_vss.loadVector(db);
-  db.serialize(() => {
-    const stmt: sqlite3.Statement = db.prepare("select vss_version()");
-    stmt.run((val,err)=>{
-      console.log("intialized sqlite db with vss version",val);
-    });
-  });
+  sqlite_vss.loadVss(db);
+  const stmt: Statement = db.prepare(infoQuery);
+  const info = stmt.get();
+  log(
+    `Intialized sqlite db: 
+    sqlite version: ${info.sqlite_version}
+    vss version: ${info.vss_version}
+    vectors preloaded: ${info.vectors}`);
+
   return db;
 }
-
 
 class SqliteVectorStore extends VectorStore {
 
@@ -46,6 +43,8 @@ class SqliteVectorStore extends VectorStore {
   }
 
   constructor(embeddings: OpenAIEmbeddings, db: Database) {
+    if (!embeddings) throw "Embeddings are required to construct a SqliteVectorStore";
+    if (!db) throw "A db is required to construct a SqliteVectorStore";
     super(embeddings,{});
     this.db = db;
     this.embeddings = embeddings;
@@ -53,8 +52,10 @@ class SqliteVectorStore extends VectorStore {
   }
 
   private createTables() {
-    this.db.exec("CREATE TABLE IF NOT EXISTS documents (id INTEGER PRIMARY KEY AUTOINCREMENT, document JSON)");
-    this.db.exec("CREATE VIRTUAL TABLE IF NOT EXISTS vss_docs USING vss0()");
+    const dimensions = 1536; // WARNING: Assumes "text-embedding-ada-002"
+    const docResult = this.db.exec("CREATE TABLE IF NOT EXISTS docs (id INTEGER PRIMARY KEY, document JSON);");
+    const vssResult = this.db.exec(`CREATE VIRTUAL TABLE IF NOT EXISTS vss_docs USING vss0( vector(${dimensions}) );`);
+    log('CREATED TABLES');
   }
 
   public async loadDocuments(documents: DocumentInput[], embeddings: OpenAIEmbeddings) {
@@ -65,45 +66,53 @@ class SqliteVectorStore extends VectorStore {
     const vectors = await embeddings.embedDocuments(docs.map(doc=>doc.pageContent));
 
     // Load the new embeddings into the database
-    const vectorInsert = this.db.prepare("INSERT INTO vss_docs (id, vector) VALUES (?, ?)");
-    const documentInsert = this.db.prepare("INSERT INTO documents ( document ) VALUES (?)");
     for (let idx = 0; idx < docs.length; idx++) {
-      const result = documentInsert.run(docs[idx]);
+      const vector = JSON.stringify(vectors[idx]);
+      const stmt = `INSERT INTO vss_docs ( rowid, vector ) VALUES ( ${idx}, '${vector}' );`;
+      log('LOAD DOCUMENT', stmt.slice(45,70));
+      const vectorInsert = this.db.prepare(stmt);
+      const result = vectorInsert.run();
       const id = result.lastInsertRowid;
-      vectorInsert.run(id, vectors[idx]);
+      const documentInsert = this.db.prepare("INSERT INTO docs ( id, document ) VALUES (?, ?);");
+      documentInsert.run(id, JSON.stringify(docs[idx]));
     }
   }
 
   public addVectors(vectors: number[][], documents: Document<Record<string, any>>[], options?: { [x: string]: any; }): Promise<void | string[]> {
-    const stmt = this.db.prepare("INSERT INTO vss_docs (vector) VALUES (?)");
-    for (const vector of vectors) {
-      stmt.run(vector);
-    }
+    /* WARN: required by parent, but doesn't do anything */
+    // const stmt = this.db.prepare("INSERT INTO vss_docs (vector) VALUES (?)");
+    // for (const vector of vectors) {
+    //   stmt.run(vector);
+    // }
     return;
   }
 
   public addDocuments(documents: Document<Record<string, any>>[], options?: { [x: string]: any; }): Promise<void | string[]> {
-    const stmt = this.db.prepare("INSERT INTO vss_docs (id, vector) VALUES (?, ?)");
-    for (const document of documents) {
-      stmt.run(document.pageContent, document.metadata);
-    }
+    /* WARN: required by parent, but doesn't do anything */
+    // const stmt = this.db.prepare("INSERT INTO docs (document) VALUES (?)");
+    // for (const document of documents) {
+    //   stmt.run(document);
+    // }
     return;
   }
 
   public similaritySearchVectorWithScore(query: number[], k: number, filter?: this["FilterType"]): Promise<[Document<Record<string, any>>, number][]> {
-    const stmt = this.db.prepare("SELECT id, vss_score(?, vector) as score FROM vss_docs ORDER BY score DESC LIMIT ?");
-    const results = stmt.all(query, k);
-    return results;
+    const stmt = `SELECT document, distance FROM vss_docs JOIN docs ON vss_docs.rowid = docs.id WHERE vss_search( vector, vss_search_params('${JSON.stringify(query)}', ${k}) );`;
+    const stmtPrepared = this.db.prepare(stmt);
+    const results = stmtPrepared.all();
+    const output = results.map(result=>[JSON.parse(result.document), result.distance]);
+    return output;
   }
 
   public save(filePath: string) {
     // Save the vector store data to a file
-    const data = this.db.export();
+    const data = this.db.serialize();
     writeFileSync(filePath, data);
+    log("SAVED");
   }
 
   public async findMatching(metadata: Metadata): Promise<Document[]>{
-    const stmt = this.db.prepare("SELECT * FROM documents WHERE document->metadata = ?");
+    const stmt = this.db.prepare("SELECT * FROM docs WHERE document->metadata = ?;");
     const result = stmt.all(metadata);
     const docs = result.map(row => new Document(row));
 
@@ -111,14 +120,23 @@ class SqliteVectorStore extends VectorStore {
   }
 
   public async deleteMatching(metadata: Metadata): Promise<number>{
-    const documentIdsSelect = this.db.prepare("SELECT id FROM documents WHERE document->metadata = ?");
-    const matching = documentIdsSelect.all(metadata);
+    const {repoPath, lastKnownCommitHash} = metadata;
+    const documentIdsSelect = this.db.prepare("SELECT id FROM docs WHERE json_extract(document, '$.metadata.repoPath') = ? AND json_extract(document, '$.metadata.commitHash') = ?;");
+    const matching = documentIdsSelect.all(repoPath, lastKnownCommitHash);
     const ids = matching.map(item=>item.id);
-    const vectorDelete = this.db.prepare("DELETE FROM vss_docs WHERE id = ?");
-    const documentDelete = this.db.prepare("DELETE FROM documents WHERE id = ?");
-    const vectorResult = vectorDelete.all(ids);
-    const documentResult = documentDelete.all(ids);
-    const count = vectorResult.changes;
+    const vectorDelete = this.db.prepare("DELETE FROM vss_docs WHERE rowid = ?;");
+    const documentDelete = this.db.prepare("DELETE FROM docs WHERE id = ?;");
+    const deleteAll = db.transaction((ids) => {
+      let result = [];
+      for (const id of ids) {
+        result.push(vectorDelete.run(id));
+        result.push(documentDelete.run(id));
+      }
+      return result;
+    });
+    
+    const results = deleteAll(ids);
+    const count = results.length / 2;
 
     return count;
   }
@@ -127,11 +145,20 @@ class SqliteVectorStore extends VectorStore {
 }
 
 export function createVectorStore(embeddings: OpenAIEmbeddings): VectorStore {
-    if(!vectorStore) {
-      if (!restoreVectorStore(embeddings)) {
-        vectorStore = new SqliteVectorStore( embeddings, {database:db} );
-      }
+    if(vectorStore) {
+      log(`EXISTS`);
+      return vectorStore;
     }
+
+    if (!restoreVectorStore(embeddings)) {
+      vectorStore = new SqliteVectorStore( embeddings, db );
+      log(`CREATED`);
+    } else {
+      log(`RESTORED`);
+    }
+
+    vectorStore.save(sqliteVecData);
+
     return vectorStore;
 }
 
@@ -139,12 +166,9 @@ function restoreVectorStore(embeddings: OpenAIEmbeddings) : boolean {
   if(!existsSync(sqliteVecData)) {
     return false;
   } else {
-    vectorStore = new SqliteVectorStore(embeddings, { database:db })
-    const data = require(`./${sqliteVecData}`);
-    const stmt = db.prepare("INSERT INTO vss_docs (doc_id, vector) VALUES (?, ?)");
-    for (const { docId, vector } of data) {
-      stmt.run(docId, vector);
-    }
+    const data = readFileSync(sqliteVecData);
+    db = initDb(data);
+    vectorStore = new SqliteVectorStore(embeddings, db)
     return true;
   }
 }
@@ -155,7 +179,11 @@ export async function addDocsToVectorstore(splitDocs: Document[]): Promise<void>
     });
     console.time(`Creating embeddings`);
   
-     await vectorStore.loadDocuments(
+    if (!vectorStore) {
+      vectorStore = createVectorStore(embeddings) as SqliteVectorStore;
+    }
+
+    await vectorStore.loadDocuments(
       splitDocs.map(({ pageContent, metadata }) => {
         return new Document({ pageContent , metadata })
       }),
@@ -171,11 +199,15 @@ export async function deleteOldEmbeddingsForDirtyFiles(
     filePaths: string[],
     m: Metadata
   ): Promise<void> {
-    const stmt = db.prepare("DELETE FROM vss_docs WHERE filePath = ? AND commitHash = ?");
-    for (const filePath of filePaths) {
-      stmt.run(filePath, m.lastKnownCommitHash);
+
+    if(!vectorStore) {
+      console.warn("SqliteVectorStore not created yet!");
+      return;
     }
-  
-    const count = stmt.changes;
+
+    const count = await vectorStore.deleteMatching(m);
+
+    await vectorStore.save(sqliteVecData);
+
     console.log(`Deleted ${count} old embeddings`);
   }
