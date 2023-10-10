ab81dab646729a62da04a184fcc1b81254cb0e33
mhykes <mhykes@thought.live>
2023-10-10 17:19:39 +0000

    do not submit devcontainer upstream

--- .devcontainer/postStart.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# Drop large files we don't need unless we are using the jetbrains editor
-sudo rm -rf /workspaces/.codespaces/shared/editors/jetbrains
-
-# Install required libraries
-sudo apt-get update
-sudo apt-get install -y libblas3 
-sudo apt-get install -y liblapack3
