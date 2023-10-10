436a5f7d6fe7fb41eb1b606637912f061cfb22e3
mhykes <mhykes@thought.live>
2023-10-10 17:03:36 +0000

    devcontainer improvements

--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 # Drop large files we don't need unless we are using the jetbrains editor
-rm -rf /workspaces/.codespaces/shared/editors/jetbrains
+sudo rm -rf /workspaces/.codespaces/shared/editors/jetbrains
 
 # Install required libraries
 sudo apt-get update
-sudo apt-get install -y libblas3 liblapack3
+sudo apt-get install -y libblas3 
+sudo apt-get install -y liblapack3
